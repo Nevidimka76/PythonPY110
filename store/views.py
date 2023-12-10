57bbb1cb0d70774a9e87d0cec5e6205475405ca5
Nevidimka76 <bakilin.dmitriyu@gmail.com>
2023-12-10 22:06:30 +0700

    123

--- store/views.py
+++ store/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views import View
 from .models import DATABASE
 
+# def shop_view(request):
+#     if request.method == "GET":
+#         with open('store/shop.html', encoding="utf-8") as f:
+#             data = f.read()  # Читаем HTML файл
+#         return HttpResponse(data)
 # Create your views here.
+
+class shopView(View):
+    def get(self,requests):
+        with open('store/shop.html', encoding="utf-8") as f:
+            data = f.read()  # Читаем HTML файл
+        return HttpResponse(data)
+        
 
 class productsView(View):
     def get(self,requests):
