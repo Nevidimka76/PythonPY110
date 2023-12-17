b32990e8b0932cf0afe843917834a05ca02ba5b1
Nevidimka76 <bakilin.dmitriyu@gmail.com>
2023-12-17 15:05:46 +0700

    123

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,re_path
-from .views import productsView,shopView
+from .views import productsView,shopView,productsPageView
 
 
     
 urlpatterns = [
 #    re_path(r'^json/(?P<d>[0-9]{1,2})/(?P<m>[0-9]{1,2})/(?P<year>[0-9]{4})/$', dateView.as_view()),
     path('product/', productsView.as_view()),
-    path('',shopView.as_view())
+    path('',shopView.as_view()),
+    path('product/<slug:page>.html',productsPageView.as_view()),
+    path('product/<int:page>', productsPageView.as_view()),
 ] 
--- store/views.py
+++ store/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
-from django.views import View
-from .models import DATABASE
 
-# def shop_view(request):
-#     if request.method == "GET":
-#         with open('store/shop.html', encoding="utf-8") as f:
-#             data = f.read()  # Читаем HTML файл
-#         return HttpResponse(data)
+from django.http import JsonResponse, HttpResponse, HttpResponseNotFound 
+from django.views import View
+from .models import DATABASE as db
+
 # Create your views here.
 
 class shopView(View):
@@ -18,6 +14,31 @@ class shopView(View):
         
 
 class productsView(View):
-    def get(self,requests):
-        return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
-                                                     'indent': 4})       
+    def get(self,rqst):
+        id_=rqst.GET.get('id')
+        if id_:
+            product=db.get(id_)
+            if product:
+                data=db[id_]
+            else:
+                return HttpResponseNotFound('Данного продукта нет в БД!')
+        else:
+            data=db
+            
+        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                     'indent': 4})  
+class productsPageView(View):
+    def get(self,reqst,page):
+        if isinstance(page,str):
+            for data in db.values():
+                if data['html']==page:
+                    with open(f'store/products/{page}.html', encoding="utf-8") as f:
+                        data = f.read()  # Читаем HTML файл
+                    return HttpResponse(data)
+        elif isinstance(page,int):
+                if data:=db.get(str(page)):
+                    with open(f"store/products/{data['html']}.html", encoding="utf-8") as f:
+                        data = f.read()  # Читаем HTML файл
+                    return HttpResponse(data)
+            
+        return HttpResponseNotFound('Описания товара нет')
