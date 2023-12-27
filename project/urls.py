95887ecae784567f07344b5a8fe0847df3ce508c
Nevidimka76 <bakilin.dmitriyu@gmail.com>
2023-12-27 19:41:22 +0700

    123

--- project/urls.py
+++ project/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('weather/',include('app_weather.urls')),
     path('',include('store.urls')),
+    path('login/', include('app_login.urls')),    
     # path('random/', random_view),
     # path('file/', fileView),
     # path('datetime/', datetime_view),
